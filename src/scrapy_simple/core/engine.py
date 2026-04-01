6098f41d5f580f82d7d9fe3553b3e0f95341e9f7
wytheli <wytheli168@163.com>
2026-04-01 12:35:21 +0800

    feat: 中间件

--- src/scrapy_simple/core/engine.py
+++ src/scrapy_simple/core/engine.py
@@ -1,3 +1,5 @@
+from scrapy_simple.middlewares.downloader_middleware import DownloaderMiddleware
+from scrapy_simple.middlewares.spider_middleware import SpiderMiddleware
 from scrapy_simple.http.request import Request
 
 from .scheduler import Scheduler
@@ -17,6 +19,8 @@ class Engine(object):
         self.scheduler = Scheduler()    # 初始化调度器对象
         self.downloader = Downloader()    # 初始化下载器对象
         self.pipeline = Pipeline()    # 初始化管道对象
+        self.spider_mid = SpiderMiddleware()    # 初始化爬虫中间件对象
+        self.downloader_mid = DownloaderMiddleware()    # 初始化下载器中间件对象
 
     def start(self):
         """启动整个引擎"""
@@ -28,17 +32,25 @@ class Engine(object):
         start_request = self.spider.start_requests()
 
         # 2. 把初始请求添加给调度器
+        # 利用爬虫中间件预处理请求对象
+        start_request = self.spider_mid.process_request(start_request)
         self.scheduler.add_request(start_request)
         # 3. 从调度器获取请求对象，交给下载器发起请求，获取一个响应对象
         request = self.scheduler.get_request()
+        # 利用下载器中间件预处理请求对象
+        request = self.downloader_mid.process_request(request)
         # 4. 利用下载器发起请求
         response = self.downloader.get_response(request)
+        # 利用下载器中间件预处理响应对象
+        response = self.downloader_mid.process_response(response)
 
         # 5. 利用爬虫的解析响应的方法，处理响应，得到结果
         result = self.spider.parse(response)
         # 6. 判断结果对象
         # 6.1 如果是请求对象，那么就再交给调度器
         if isinstance(result, Request):
+            # 利用爬虫中间件预处理请求对象
+            result = self.spider_mid.process_request(result)
             self.scheduler.add_request(result)
         # 6.2 否则，就交给管道处理
         else:
